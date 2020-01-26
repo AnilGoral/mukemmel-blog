4822d119cc849b1e1b74465d9be4e8b3a8f1b954
AnilG <56311636+AnilGoral@users.noreply.github.com>
2020-01-26 13:32:33 +0300

    Blog İskeleti
    *Yazı Planı Eklendi
    *Navbar eklendi ama değişiklik yapılacak

---
+++ Blog/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Anıl Göral</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-xl bg-light justify-content-center"> ...
+	</nav>
+	<h2  id="main-title">Anıl Göral</h2>
+	<div class="container p-3 my-3 bg-primary text-white">
+		<div class="span-6">
+			<article class="p-2 my-2 bg-white text-primary" id="blog-post">
+				<p id="title">Sa</p>
+				<p id="text">herkese merhaba arkadaşlar</p>
+				<p id="date">08.11.2019</p>
+				<a id="contiune-reading" href="#">Devamını Oku</a>
+			</article>
+			<article class="p-2 my-2 bg-white text-primary" id="blog-post">
+				<p id="title">Sa</p>
+				<p id="text">herkese merhaba arkadaşlar</p>
+				<p id="date">08.11.2019</p>
+				<a id="contiune-reading" href="#">Devamını Oku</a>
+			</article>
+			<article class="p-2 my-2 bg-white text-primary" id="blog-post">
+				<p id="title">Sa</p>
+				<p id="text">herkese merhaba arkadaşlar</p>
+				<p id="date">08.11.2019</p>
+				<a id="contiune-reading" href="#">Devamını Oku</a>
+			</article>
+			<article class="p-2 my-2 bg-white text-primary" id="blog-post">
+				<p id="title">Sa</p>
+				<p id="text">herkese merhaba arkadaşlar</p>
+				<p id="date">08.11.2019</p>
+				<a id="contiune-reading" href="#">Devamını Oku</a>
+			</article>
+			<article class="p-2 my-2 bg-white text-primary" id="blog-post">
+				<p id="title">Sa</p>
+				<p id="text">herkese merhaba arkadaşlar</p>
+				<p id="date">08.11.2019</p>
+				<a id="contiune-reading" href="#">Devamını Oku</a>
+			</article>
+
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
